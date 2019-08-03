f510506cedc18464026c24414b82e6960354dca8
Raven Gates <ravengates@icloud.com>
2019-08-03 08:30:30 -0500

    more htmls made, js file updated with more brewery info, styling html

--- Assets/Javascript/brews.js
+++ Assets/Javascript/brews.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
     $('#old-enough').hide();
     $('#sorry').hide();
     $('#footer').hide();
+    $('#results').hide();
+
 
 
 });
@@ -19,6 +21,8 @@ $('#yesButton').on('click', function () {
     $('#sorry').hide();
     $('#old-enough').show();
     $('#footer').show();
+    $('#results').hide();
+
 
 });
 
@@ -27,6 +31,8 @@ $('#noButton').on('click', function () {
     $('#old-enough').hide();
     $('#sorry').show();
     $('#footer').hide();
+    $('#results').hide();
+
 
 
 });
@@ -35,14 +41,7 @@ $('#noButton').on('click', function () {
 
 
 
-//click start button -- needs to show questions, start timer, intake answers.
-//$('#yesButton').on('click', function(){
-//   $('#startBtn').hide();
-//   unHide(); 
-//   startTimer();
-//$('.results').hide();
 
-//});
 
 
 
@@ -277,19 +276,508 @@ let breweriesCollection = [
                     ibuRank: "medium",
                     info: "GrIN is an experimental “cocktail beer” designed to mimic the flavors and experience of a gin and tonic. A golden ale base beer brewed with copious amounts of Juniper and Coriander sets the foundation for this concoction. We then blended the pungent beer with locally sourced tonic water and lime supplied by Oak Cliff Beverage Works.When the Golden Ale and tonic come together, a beautiful balance is achieved and a unique beer drinking experience is realized. If you think your mind’s playing tricks on you, have another. At 4% ABV, GrIN will allow you to have the gin and tonic experience without the repercussions of overindulging on the real thing."
                 }
-    
+
+                
+        ]
+    },
+
+    { name: "On Rotation",
+    info: "On Rotation’s “craft beer laboratory” feel was designed to appeal to all levels of craft beer enthusiast, from the experienced craft beer drinker or homebrewer to those just starting their journey into craft beer.",
+    url: "https://on-rotation.com",
+    position: { lat: 32.8109335, lng: -96.731396 },
+    beers: [
+        {
+            name: "Jalapeno Saison",
+            keywords: "Saison",
+            style: "Chili Saison",
+            abv: 8.1,
+            abvRank: "high",
+            ibu: 25,
+            ibuRank: "medium",
+            info: "Never fear. The spice is here. This dry, chili saison's bark is worse than its bite with a strong jalapeño nose balanced by a tortilla chip breadiness and a dry finish."
+        },
+        
+
+        {
+            name: "Cafe Au Lait",
+            keywords: ["stout"],
+            style: "Chicory Coffee Milk",
+            abv: 6.6,
+            abvRank: "medium",
+            ibu: 45,
+            ibuRank: "medium",
+            info: "Based on a New Orleans café au lait, this milk stout is big on black coffee flavor with a pleasant bitterness from subtle, spicy hops and a generous addition of cold brew coffee and chicory."
+        },
+        {
+            name: "Zero Poms Given",
+            keywords: ["ipa"],
+            style: "American",
+            abv: 6,
+            abvRank: "medium",
+            ibu: 0,
+            ibuRank: "low",
+            info: "Brewed with no hops in the boil, this zero IBU IPA dry hopped with Vic Secret and Huell Melon fermented on a very aggressive yeast strain known for a honey-like aroma and notes of overripe mango. "
 
             
-        ]
-    }
-]
+        
+    },
+    {
+        name: "Heimdall's Vision",
+        keywords: ["ipa"],
+        style: "New England",
+        abv: 8,
+        abvRank: "high",
+        ibu: 19,
+        ibuRank: "low",
+        info: "This all-seeing protector of a hazy IPA features Citra, Huell Melon, and Pacifica hops, which impart notes of citrus, lime, honeydew melon, and strawberry."
+    },
+    {
+        name: "Bigger Bam Boom",
+        keywords: ["stout"],
+        style: "Rye Whiskey Barrel-Aged Imperial Brown Sugar Oatmeal",
+        abv: 11.8,
+        abvRank: "very high",
+        ibu: 36,
+        ibuRank: "medium",
+        info: "Aged for more than 10 months in a rye whiskey barrel, this bigger, badder batch of Big Bam Boom is a real maneater."
+    },
+    {   name: "Hot to Coco",
+        keywords: ["American Porter"],
+        style: "American Porter with Coconut & Chipotle Peppers",
+        abv: 6.4,
+        abvRank: "low",
+        ibu: 39,
+        ibuRank: "medium",
+        info: "Infused with toasted coconut and dried chipotle peppers, this American porter plays both sides of the sweet and spicy."
+    },
+    {   name: "I Understand NOTHING",
+        keywords: ["ipa"],
+        style: "sour",
+        abv: 6.7,
+        abvRank: "low",
+        ibu: 31,
+        ibuRank: "medium",
+        info: "Michael Scott doesn't get this beer. Light in body and with Citra hops in the boil, we added lactose (milk sugar) to this mixed culture sour IPA to add a subtly sweet, smooth quality."
+    
+    },
+    {   name: "Belgain Conspiracy",
+        keywords: ["pale ale"],
+        style: "belgian",
+        abv:7.6,
+        abvRank: "high",
+        ibu: 26,
+        ibuRank: "medium",
+        info: "Naruto run, don't walk, to this beer. Copper in color with an aromatic and simple grain base, this Belgian pale ale features a prominent malt backbone with a light dose of Idaho #7 hops, which provide a nice citrus character with some background earthiness."
+    },
+    {   name: "Last Stand of the Warriors Three",
+        keywords: ["ipa"],
+        style: "new england",
+        abv:8,
+        abvRank: "high",
+        ibu: 19,
+        ibuRank: "low",
+        info:"Brewed in honor of Asgardians Fandral, Hogun, and Volstagg, this hazy IPA, like its sister brew Heimdall's Vision, features Citra, Huell Melon, and Pacifica hops"
+},
+{       
+        name: "I Woke Up Like This ",
+        keywords: ["Witbier"],
+        style: "Witbier",
+        abv:8,
+        abvRank: "high",
+        ibu: 11,
+        ibuRank: "low",
+        info: "Roll out of bed and enjoy this Mimosa Wit. Brewed with primarily wheat and all Citra hops, it's weighty in body with a major citrus note."
+},
+{       
+       name: "Raspberry Tart",
+       keywords: ["Sour Saison"],
+       style: "Sour Saison with Raspberry",
+       abv:8.8,
+       abvRank: "high",
+       ibu: 33,
+       ibuRank: "medium",
+       info: "This wheat-heavy saison is kettle-soured to crisp, tart perfection then hopped exclusively with Belma hops and fermented on a clean, French saison yeast."
+},
+
+{ 
+    name: "Never Jaded",
+    keywords: ["Dry-Hopped Saison"],
+    style: "Dry-Hopped Saison",
+    abv:7.9,
+    abvRank: "high",
+    ibu:24,
+    ibuRank: "medium",
+    info:"Dry hopped exclusively with New Zealand Pacific Jade hops, known for their soft, subtle bitterness, this light and crisp saison is full of fresh lemon with a pleasant bready aroma and a dry, citrus twist to the finish."
+
+},
+
+{ 
+    name: "Cryo Treatment ",
+    keywords: ["ipa"],
+    style: "Imperial / Double",
+    abv:9.6,
+    abvRank: "high",
+    ibu: 101,
+    ibuRank: "very high",
+    info:"With 101 IBUs, this dry-hopped double IPA featuring Simcoe, Ekuanot, Sticklebract, and Simcoe Cryo hops will put ice in your veins. "
+
+}]
+
+},
+
+{
+name: "Braindead Brewing",
+info: "",
+url: "http://braindeadbrewing.com/",
+position: { lat: 32.7838067, lng: -96.7854202 },
+beers: [
+    {
+        name: "Gritz",
+        keywords: "blonde ale",
+        style: "Pre-prohibition Cream Ale ",
+        abv: 4.4,
+        abvRank: "low",
+        ibu: 23,
+        ibuRank: "medium",
+        info: "An approachable yet obscure light blonde ale style descended from old world golden lagers."
+    },
+    {
+        name: "HONEY LAGER (CORE BEER)",
+        keywords: "American Lager",
+        style: " American Lager with Texas Wildflower Honey ",
+        abv: 5.6,
+        abvRank: "medium",
+        ibu: 25,
+        ibuRank: "medium",
+        info: "Started from the bottom fermentation, now we're here. The ultimate warm weather beer, pairs well with everything."
+
+    },
+    {
+        name: "HAPPINESS COMES FROM WITHIN (CORE BEER)",
+        keywords: "ipa",
+        style: "hazey ipa",
+        abv: 6.7,
+        abvRank: "medium",
+        ibu: 30,
+        ibuRank: "medium",
+        info: "Soft mouthfeel with a nice OJ acidity on the back end. Tropical and easy to drink. For the Hazebois."
+        },
+    {
+        name: "CERVEZA OSCURA (CORE BEER)",
+        keywords: "Lager",
+        style: "Mexican Dark Lager",
+        abv: 4.7,
+        abvRank: "medium",
+        ibu: 16,
+        ibuRank: "low",
+        info: "The candied sugar thrown in gives this a subtle sweet note that brings back memories of beaches, bonfires, and micheladas."
+    },
+    {
+        name: "BASKET CATCH",
+        keywords: "Pale Ale",
+        style: "Dry Hopped American Pale Ale",
+        abv: 4.7,
+        abvRank: "medium",
+        ibu: 16,
+        ibuRank: "low",
+        info: "not available"
+      
+    },
+    {
+        name: "FOREIGN EXPORT STOUT (CORE BEER)",
+        keywords: "Stout",
+        style: "Oatmean Extra Stout",
+        abv: 6.6,
+        abvRank: "medium",
+        ibu: 43,
+        ibuRank: "low",
+        info: "A complex black ale reminiscent of dark chocolate covered espresso beans & currants. Built on dark and caramel barley males with fruity American hops & a hint of licorice root."
+    },
+    {
+        name: "GIVEN TO RYE",
+        keywords: "Brown Ale",
+        style: "American Rye Brown Ale",
+        abv: 4.9,
+        abvRank: "medium",
+        ibu: 45,
+        ibuRank: "low",
+        info: "Malty, rye forward, brown ale with notes of coffee, tobacco and dark brown sugar. Slightly boozy with long, dry finish."
+    },
+    {
+        name: "TIBRADDEN STOUT",
+        keywords: "Stout",
+        style: "Stout-Irish Dry ",
+        abv: 8.8,
+        abvRank: "high",
+        ibu: 30,
+        ibuRank: "low",
+        info: "Dry Rye Stout with Czech hops aged in whiskey casks."
+    },
+    {
+        name: "RIGHT IN MY WHEELHOUSE",
+        keywords: "Pilsner",
+        style: "Tropical Style Pilsner",
+        abv: 4.5,
+        abvRank: "low",
+        ibu: 30,
+        ibuRank: "low",
+        info: "not available"
+    },
+    {
+        name: "RISKY CLIQUE",
+        keywords: "Ale",
+        style: "Dry Hopped Sour Ale",
+        abv: 7,
+        abvRank: "high",
+        ibu: 15,
+        ibuRank: "low",
+        info: "not available"
+    },
+    {
+        name: "AGAVE WHEAT",
+        keywords: "Pale Ale",
+        style: "Dry Hopped American Pale Ale",
+        abv: 5.8,
+        abvRank: "medium",
+        ibu: 15,
+        ibuRank: "low",
+        info: "Summer's almost here so we made the perfect hot weather wheat ale."
+    },
+    {
+        name: "THE PIPER AT THE GATES OF DAWN",
+        keywords: "Ale",
+        style: "Coffee Scotch Ale",
+        abv: 7,
+        abvRank: "high",
+        ibu: 24,
+        ibuRank: "low",
+        info: "One of our highest rated beers of all time, malty and roasted coffee notes dominate with a lingering copper finish."
+    },
+    {
+       name: "PRIVATE SNACKS: 4TH ANNIVERSARY",
+        keywords: "Stout",
+        style: "Golden Imperial Stout",
+        abv: 11,
+        abvRank: "high",
+        ibu: 45,
+        ibuRank: "medium",
+        info:"Wow. 4 years. What do we say except thank you?! A flip on a traditional imperial stout, sweet and stocky."
+    },
+    {
+        name: "SOMETHING SEXY",
+        keywords: "Stout",
+        style: "Stout",
+        abv: 8,
+        abvRank: "high",
+        ibu: 54,
+        ibuRank: "high",
+        info: "Brewed in collaboration with Driveway Brewing, the winner of our 2017 Homebrew Contest ramped up for a 15bbl system. Sweet, silky, romantic."
+    },
+    {
+        name: "WATCHFUL PROTECTOR",
+        keywords: "Stout",
+        style: "Stout",
+        abv:15.5,
+        abvRank: "very high",
+        ibu: 88,
+        ibuRank: "very high",
+        info: "Triple reiterated mashed We Own The Night - Coffee Oatmeal Molasses Imperial Stout aged in 1 Lone Elm Wheat Whiskey Barrel"
+    },
+    {
+        name: "ORDINARY BITTER",
+        keywords: "Pale Ale",
+        style: "Dry Hopped American Pale Ale",
+        abv: 4.1,
+        abvRank: "low",
+        ibu: 25,
+        ibuRank: "medium",
+        info: "A British classic using East Kent Golding hops, malt forward and lean. Perfect for a pint with the boys."
+    },
+    {
+        name: "MAJOR FROG",
+        keywords: "ipa",
+        style: "Green Bullet Double IPA",
+        abv: 10,
+        abvRank: "high",
+        ibu: 90,
+        ibuRank: "very high",
+        info:"Major Frog is our Double IPA category, a bump up in ABV and sometimes in IBUs from the Army of Frogs category."
+    },
+    {
+        name: "LOVE CULT",
+        keywords: "Pale Ale",
+        style: "Pale Ale New England",
+        abv: 4.7,
+        abvRank: "low",
+        ibu: 30,
+        ibuRank: "medium",
+        info: "not available"
+    },
+    {
+        name: "ORTHODOX IMPOSTER",
+        keywords: "Belgian Blonde",
+        style: "Belgian Blonde",
+        abv: 6.2,
+        abvRank: "low",
+        ibu: 37,
+        ibuRank: "medium",
+        info:"Abbey style Belgian Single brewed with American Pale Ale, American 2 Row, American White Wheat & German Dark Wheat malts, Cane Sugar, Styrian Bobek, Czech Kazbeck & Apollo hops,House Belgian Yeast Blend"
+    },
+    {
+        name: "WE ARE YOUR OVERLORDS",
+        keywords: "Pale Ale",
+        style: "Dry Hopped American Pale Ale",
+        abv: 11.4,
+        abvRank: "very high",
+        ibu: 70,
+        ibuRank: "high",
+        info: "It was the origin for the American Brown Ale style."
+    },
+    {
+        name: "MAIN ST. KOLSCH",
+        keywords: "Kolsch",
+        style: "Kolsch",
+        abv: 4.9,
+        abvRank: "low",
+        ibu: 24,
+        ibuRank: "medium",
+        info:"Golden, malty and refreshing."
+    },
+    {
+        name: "APPALLINGLY IRONIC OUTCOME",
+        keywords: "Golden Ale",
+        style: "Belgian Strong Golden Ale ",
+        abv: 10,
+        abvRank: "high",
+        ibu: 26,
+        ibuRank: "medium",
+        info: "Idle Playthings aged in Chardonnay barrels for 6 months."
+    },
+    {
+        name: "DAYSHINE SUNDREAM",
+        keywords: "Belgian Wit ",
+        style: "Belgian Wit w/Candied Ginger ",
+        abv: 5.8,
+        abvRank: "low",
+        ibu: 21,
+        ibuRank: "medium",
+        info: "As light as it is refreshing, the candied ginger adds a slight spice that cuts the full mouthfeel caused by the wheat malts."
+
+    },
+    {
+        name: "IDLE PLAYTHINGS",
+        keywords: "Belgian Golden Strong",
+        style: "Belgian Golden Strong",
+        abv: 8.5,
+        abvRank: "high",
+        ibu: 26,
+        ibuRank: "medium",
+        info: "Sweet tropical fruits, bright and refreshing."
+    },
+    
+    {
+        name: "GEISTBIER",
+        keywords: "Altbier",
+        style: "Altbier",
+        abv: 10.6,
+        abvRank: "high",
+        ibu: 52,
+        ibuRank: "high",
+        info: "Our Fruhlingzeit Altbier aged in Witherspoon bourbon barrels. The most fitting beer to mark 10 years since our brewmaster first started washing kegs and cleaning draft lines at Morgan Street Brewing."
+    },
+    {
+        name: "FROG IPA",
+        keywords: "ipa",
+        style: "smash ipa",
+        abv: 6-8,
+        abvRank: "high",
+        ibu: 50-99,
+        ibuRank: "high",
+        info: "Our rotating single hop IPA series. Check at the brewery for the hop of the moment!"
+    },
+    {
+        name: "BEÓR TUNNE",
+        keywords: "Ale",
+        style: "Olde Ale",
+        abv: 10,
+        abvRank: "high",
+        ibu: 40,
+        ibuRank: "medium",
+        info: "Malty, Earthy, red wine, wood and funky nose. Big malt flavor, dark fruit, rustic bread, earthy funk flavor."
+    },
+    {
+        name: "MEMORY HOLE",
+        keywords: "Barleywine",
+        style: "American Barleywine",
+        abv: 10.9,
+        abvRank: "high",
+        ibu: 85,
+        ibuRank: "high",
+        info: "Generous toffee and estery fruit sweetness on the nose. The first taste is a sublime mix of earthy oak and sharp citrusy bitterness, wrapped together in a slightly warm alcohol burn. The oak is a lingering friend: refusing to leave after the party but helps you clean up so it's okay by you. A true American take on the English strong ale."
+    },
+    {
+        name: "GOLDEN LADDER",
+        keywords: "Lager",
+        style: "Apple Brandy-barrel aged Mega Helles Lager",
+        abv: 14.4,
+        abvRank: "very high",
+        ibu: 30,
+        ibuRank: "medium",
+        info: "Our barrel-aged version of Welcome to Helles, we took an already absurdly good beer and took it up a notch. Sweet and malty blend like it should, with a spicy orchard note."
+    },
+    {
+        name: "SPEARMINT OF DESTINY",
+        keywords: "Stout",
+        style: "Stout",
+        abv: 9.1,
+        abvRank: "high",
+        ibu: 43,
+        ibuRank: "medium",
+        info: "Tastes like frolicking through an orchard of mint on a dark chocolate night."
+    },
+    {
+        name: "DARTH LAMBICUS",
+        keywords: "Ale",
+        style: "Oak Bourbon Barrel Aged Belgian Dark Fruited Strong Ale",
+        abv: 8.8,
+        abvRank: "high",
+        ibu: 31,
+        ibuRank: "medium",
+        info: "AA bold Belgian dark strong ale with tart cherry puree, you'll notice the sweet bourbon sting make way for a mellow dark plum and cherry aftertaste. "
+    },
+        
+    {
+        name: "HAMMER OF THE GODS",
+        keywords: "Pale Ale",
+        style: "Dry Hopped American Pale Ale",
+        abv: 11.1,
+        abvRank: "very high",
+        ibu: 65,
+        ibuRank: "high",
+        info: "An Imperial Wheat Porter aged in Woodford Reserve and Stranahan's barrels. Unlike most barrel-aged offerings, Hammer of the Gods was designed from the ground up to be whiskey barrel aged. The inclusion of wheat adds a soft pie-crust character to a big, milk-chocolaty wake up call for your mouth."
+
+    }]
+
+
+    }]
 
 
 
+// ABV: low: up to and including 4%
+// medium: up to 7%
+//     high: 7-10%
+//     Very high: above 10%
+
+// IBU Scale: 	low: >=20
+//         medium: 21 - 50
+//     high: 51-80
+//     Very High: Above 80
 
 
 $("#submitem").on("click", function () {
     event.preventDefault()
+    $('#results').show();
+
 
     ibu = $("#ibu").val().trim();
     abv = $("#abv").val().trim();
